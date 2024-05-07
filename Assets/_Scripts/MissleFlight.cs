543aa9d9864594ca429155098959d60e647dcd37
Alexander Evstratov <alexdrugev@me.com>
2024-05-07 12:25:36 +0300

    Add good explosion.

--- Assets/_Scripts/MissleFlight.cs
+++ Assets/_Scripts/MissleFlight.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MissleFlight : MonoBehaviour
 {
     [SerializeField] private GameObject _badExplosion;
+    [SerializeField] private GameObject _goodExplosion;
 
     private float _speed = 10.0f;
 
@@ -18,6 +19,14 @@ public class MissleFlight : MonoBehaviour
             GameObject badExplosion = Instantiate(_badExplosion);
             badExplosion.transform.position = transform.position;
             Destroy(badExplosion, 0.5f);
+            Destroy(gameObject);
+        }
+
+        else if (collision.gameObject.CompareTag("Border"))
+        {
+            GameObject goodExplosion = Instantiate(_goodExplosion);
+            goodExplosion.transform.position = transform.position;
+            Destroy(goodExplosion, 0.5f);
             Destroy(gameObject);
         }
     }
