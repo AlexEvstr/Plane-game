98173569cf18760a778bab25371c7bd524d0b049
Alexander Evstratov <alexdrugev@me.com>
2024-05-07 11:45:39 +0300

    Add badExposion.

--- Assets/_Scripts/MissleFlight.cs
+++ Assets/_Scripts/MissleFlight.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class MissleFlight : MonoBehaviour
 {
-    private float _speed = 5.0f;
+    [SerializeField] private GameObject _badExplosion;
+
+    private float _speed = 10.0f;
 
     private void Update()
     {
         transform.Translate(Vector2.up * Time.deltaTime * _speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Construct"))
+        {
+            GameObject badExplosion = Instantiate(_badExplosion);
+            badExplosion.transform.position = transform.position;
+            Destroy(badExplosion, 0.5f);
+            Destroy(gameObject);
+        }
     }
 }
